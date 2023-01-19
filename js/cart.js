a630ede19bf5dbc6796b4f3a011a3171a270fb0b
Simon Agudelo <simonagudelo93@gmail.com>
2023-01-19 00:24:57 -0500

    adding actions add and delete, plus remove counter when items are in 0

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,14 @@
-let addProductsToCart = () =>{
+let addProductsToCart = () => {
     let addProductBtn = document.querySelectorAll(".add-product");
     let carritoButton = document.getElementById("carritoButton");
     let carritoProductos = document.getElementById('carritoProductos')
     let carritoObj = {}
     let nProductos = 0
-    
+
 
     addProductBtn = [...addProductBtn];
     addProductBtn.forEach(productBtn => {
-        productBtn.addEventListener("click", (e)=>{
+        productBtn.addEventListener("click", (e) => {
             //capturar el parent element de donde se hizo click para
             // luego capturar el id
             let productContainer = e.target.parentNode.parentNode;
@@ -16,23 +16,23 @@ let addProductsToCart = () =>{
         })
     });
 
-    
+
     //captura el parent con el id del card del producto
     let crearCarrito = (objeto) => {
 
         const productoCapturado = {
-        id: objeto.id,
-        nombre: objeto.querySelector('.product-name').textContent,
-        precio: objeto.querySelector('.price').textContent,
-        img: objeto.querySelector('.img').getAttribute('src'),
-        cantidad: 1
+            id: objeto.id,
+            nombre: objeto.querySelector('.product-name').textContent,
+            precio: objeto.querySelector('.price').textContent,
+            img: objeto.querySelector('.img').getAttribute('src'),
+            cantidad: 1
         }
 
-        if(carritoObj.hasOwnProperty(productoCapturado.id)){
+        if (carritoObj.hasOwnProperty(productoCapturado.id)) {
             productoCapturado.cantidad = carritoObj[productoCapturado.id].cantidad + 1
         }
 
-        carritoObj[productoCapturado.id] = {...productoCapturado}
+        carritoObj[productoCapturado.id] = { ...productoCapturado }
         pintarProductos()
     }
 
@@ -48,54 +48,91 @@ let addProductsToCart = () =>{
                 <td class="col-50 text-end">${(parseInt(producto.precio) * parseInt(producto.cantidad))}</td>
                 <td class="col-100 text-center">
                     <div class="d-flex justify-content-center align-items-center mx-auto gap-2">
-                        <button class="btn btn-outline-primary d-flex justify-content-center align-items-center">
-                            <span class="d-block mb-1">+</span>
+                        <button id="addItemBtn" data-id="${producto.id}" class="btn btn-outline-primary d-flex justify-content-center align-items-center">
+                            +
                         </button>
-                        <button class="btn btn-outline-danger d-flex justify-content-center align-items-center">
-                            <span class="d-block mb-1">-</span>
+                        <button id="removeItemBtn" data-id="${producto.id}" class="btn btn-outline-danger d-flex justify-content-center align-items-center">
+                           -
                         </button>
                     </div>
                 </td>
             </tr>
             `
         })
-        nProductos =  Object.values(carritoObj).reduce((acc, { cantidad }) => acc + cantidad, 0)
+        nProductos = Object.values(carritoObj).reduce((acc, { cantidad }) => acc + cantidad, 0)
 
         counterFunction(nProductos)
+
     }
 
-    // agregar prouductos al counter 
+    // agregar prouductos al counter
     let counterFunction = (nProductos) => {
-    let carrito = document.getElementById("carrito");
-    let counterHTML = `<div id="counter" class="counter position-absolute rounded-circle"></div>`
-     console.log(nProductos);
+        let carrito = document.getElementById("carrito");
+        let counterHTML = `<div id="counter" class="counter position-absolute rounded-circle"></div>`
+        console.log(nProductos);
 
-        if(nProductos !== 0){
+        if (nProductos !== 0) {
             //carrito desabilitado si no hay productos seleccionados
             carritoButton.classList.remove('disabled')
             carrito.innerHTML += counterHTML;
-    
+
             //pasar el numero de carrito al icono de counter en el html
             let counterId = document.getElementById("counter");
             counterId.textContent = nProductos;
+            console.log ("nproducts " + nProductos)
 
             vaciarCarritoFunction(counterId)
         }
     }
 
     // crear btn para vaciar carrito y counter
-    let vaciarCarritoFunction = (counterId)=> {
+    let vaciarCarritoFunction = (counterId) => {
         // crear el btn de vacial el carrito
         let vaciarCarrito = document.getElementById('carritoModalFooter');
         let vaciarBtn = `<button id="vaciarCarrito" type="button" class="btn btn-danger">Vaciar Carito</button></div>`
         vaciarCarrito.innerHTML = vaciarBtn
 
         let btnEvent = document.getElementById('vaciarCarrito')
-        btnEvent.addEventListener("click", ()=>{
+        btnEvent.addEventListener("click", () => {
             carritoObj = {}
             counterId.textContent = ""
             pintarProductos()
         })
+
+        let addItemBtn = document.querySelectorAll('.btn-outline-primary')
+        let removeItemBtn = document.querySelectorAll('.btn-outline-danger')
+
+        addItemBtn.forEach(btn => {
+            btn.addEventListener("click", (e)=>{
+                addRemoveItems(e, counterId)
+            })
+        })
+        removeItemBtn.forEach(btn => {
+            btn.addEventListener("click", (e)=>{
+                addRemoveItems(e, counterId)
+            })
+        })
     }
 
+    
+
+
+    let addRemoveItems = (e, counterId) => {
+        //aumentar cantidad
+        if(e.target.id === "addItemBtn"){
+            let producto = carritoObj[e.target.getAttribute('data-id')]
+            producto.cantidad ++
+            carritoObj[e.target.getAttribute('data-id')] = {...producto }
+            pintarProductos()
+        }else if(e.target.id === "removeItemBtn"){
+            let producto = carritoObj[e.target.getAttribute('data-id')]
+            producto.cantidad --
+
+            if(producto.cantidad === 0 ){
+                delete carritoObj[e.target.getAttribute('data-id')]
+                counterId.textContent= ""
+            }
+            pintarProductos()
+        }
+    }
 }
